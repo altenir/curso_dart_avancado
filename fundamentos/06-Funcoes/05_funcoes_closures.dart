dc04bbf8894d41aa7d5b788cbf1b724ffce9c6ab
altenir <altenir30@gmail.com>
2021-04-03 00:27:49 -0300

    closures

---
+++ fundamentos/06-Funcoes/05_funcoes_closures.dart
@@ -0,0 +1,84 @@
+///
+/// Uma Closure ocorre quando uma função é declarada dentro do corpo de outra função!
+///
+funcaoClosures() {
+  print('06.5.1) Clousures sem Retorno\n');
+
+  var saudacao = (String nome) {
+    var mensagem = (complemento) => print('Olá $nome! $complemento');
+    mensagem('Seja bem vindo');
+  };
+  print(saudacao);
+  saudacao('Fernando');
+
+  print('06.5.1) Clousures com Retorno\n');
+
+  Function somar(int valorA) {
+    print(valorA);
+    return (double valorB) {
+      print(valorB);
+      return valorA + valorB;
+    };
+  }
+
+  var somarDez = somar(10);
+  print(somarDez(5.0));
+
+  Function porcentagem(desconto) => (valor) => desconto * valor;
+  var descontarDez = porcentagem(.9);
+
+  var descontarVinte = porcentagem(.8);
+  print(descontarDez(100));
+  print(descontarVinte(200));
+
+  print('06.5.1) Clousures com Objetos\n');
+  var novoObjeto = () {
+    var id = 0;
+    var objetoCriado = (String nome, descricao) {
+      // return 'id: ${(++id).toString().padLeft(2, '0')} nome: $nome, descrição: $descricao'; // retorna array
+      // return {'id': (++id).toString().padLeft(2, '0'), 'nome': nome, descrição': descricao}; // retorna Map
+
+      return Objeto.fromMap({
+        'id': (++id).toString().padLeft(2, '0'),
+        'nome': nome,
+        'descrição': descricao
+      }); // retorna um Objeto
+    };
+    return objetoCriado;
+  };
+
+  var objeto = novoObjeto();
+  print(objeto);
+
+  var listaObjetos = [objeto('Fernando', 1.99)];
+  listaObjetos.add(objeto('iPhone', 3000.00));
+  listaObjetos.add(objeto('Fones', 100));
+
+  for (var objeto in listaObjetos) {
+    // print(objeto);
+    // print(objeto.substring(7));
+    // print(objeto.map((c, v) => MapEntry(c, (v is double) ? '${descontarDez(v)}' : v)));
+    print((objeto.descricao is num)
+        ? descontarVinte(objeto.descricao)
+        : objeto.descricao);
+  }
+}
+
+class Objeto {
+  String? id, nome;
+  dynamic descricao;
+
+  Objeto({this.id, this.nome, this.descricao});
+
+  factory Objeto.fromMap(Map<String, dynamic> map) {
+    return Objeto(
+      id: map['id'].toString(),
+      nome: map['nome'],
+      descricao: map['descricao'],
+    );
+  }
+}
+
+void main() {
+  funcaoClosures();
+}
