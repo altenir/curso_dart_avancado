11bed97d0e610c50233003b6ba36b8a77dbcf878
altenir <altenir30@gmail.com>
2021-04-03 17:05:38 -0300

    modificadores

---
+++ fundamentos/10-Modificadores/01_static.dart
@@ -0,0 +1,5 @@
+/**
+ * Static
+ * A varivel estatica fica disponível somente na própria classe em vez de suas instancias/objetos
+ * Em métodos estaticos devemos usar variáveis estáticas da classe
+ */
